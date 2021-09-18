dea3bebf93e83d3ba4b29603a3e5b46599b1cc69
Juan pablo <juanpabloperezdevelopment@gmail.com>
2021-09-17 20:01:13 -0500

    Adding first commit

---
+++ scalaml/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for scalaml project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scalaml.settings')
+
+application = get_asgi_application()
