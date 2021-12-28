24c9c7f24777a9d9469517ac38ff2544454d8220
Juan pablo <juanpabloperezdevelopment@gmail.com>
2021-12-27 21:36:08 -0500

    Adding changes 27-12-2021

--- apps/tipos_variables/urls.py
+++ apps/tipos_variables/urls.py
@@ -6,4 +6,6 @@ app_name = 'tipos_variables'
 urlpatterns = [
     path('tipos_variables', tipos_variables.as_view(), name = 'main'),
     path('tipos_variables/ejemplo1', tipo_variables_ejemplo1.as_view(), name = 'ejemplo1'),
+    path('tipos_variables/ejemplo2', tipo_variables_ejemplo2.as_view(), name = 'ejemplo2'),
+    path('tipos_variables/ejemplo3', tipo_variables_ejemplo3.as_view(), name = 'ejemplo3'),
 ]
--- apps/tipos_variables/views.py
+++ apps/tipos_variables/views.py
@@ -15,6 +15,27 @@ class tipo_variables_ejemplo1(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['example'] = True
+        context['num_example'] = 1
+        context['page'] = 2
+        return context
+
+class tipo_variables_ejemplo2(TemplateView):
+    template_name = "tipos-variables/ejemplos/ejemplo2.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['example'] = True
+        context['num_example'] = 2
+        context['page'] = 2
+        return context
+
+class tipo_variables_ejemplo3(TemplateView):
+    template_name = "tipos-variables/ejemplos/ejemplo3.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['example'] = True
+        context['num_example'] = 3
         context['page'] = 2
         return context
     
