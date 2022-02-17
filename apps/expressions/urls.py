3d0f52a9e07d0623d28ff6a369626326a0735867
Juan pablo <juanpabloperezdevelopment@gmail.com>
2022-02-16 21:15:06 -0500

    Adding new features in firts module

--- apps/expressions/urls.py
+++ apps/expressions/urls.py
@@ -5,4 +5,8 @@ app_name = 'expressions'
 
 urlpatterns = [
     path('expresiones', expressions.as_view(), name = 'main'),
+    path('expresiones/ejemplo1', ejemplo1.as_view(), name = 'ejemplo1'),
+    path('expresiones/ejemplo2', ejemplo2.as_view(), name = 'ejemplo2'),
+    path('expresiones/ejemplo3', ejemplo3.as_view(), name = 'ejemplo3'),
+    path('expresiones/ejemplo4', ejemplo4.as_view(), name = 'ejemplo4'),
 ]
--- apps/expressions/views.py
+++ apps/expressions/views.py
@@ -7,3 +7,45 @@ from django.views.generic import(
 
 class expressions(TemplateView):
     template_name = "expressions/index.html"
+
+class ejemplo1(TemplateView):
+    template_name = "expressions/ejemplos/ejemplo1.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['example'] = True
+        context['num_example'] = 1
+        context['page'] = 1
+        return context
+
+class ejemplo2(TemplateView):
+    template_name = "expressions/ejemplos/ejemplo2.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['example'] = True
+        context['num_example'] = 2
+        context['page'] = 1
+        return context
+
+
+class ejemplo3(TemplateView):
+    template_name = "expressions/ejemplos/ejemplo3.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['example'] = True
+        context['num_example'] = 3
+        context['page'] = 1
+        return context
+
+class ejemplo4(TemplateView):
+    template_name = "expressions/ejemplos/ejemplo4.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['example'] = True
+        context['num_example'] = 4
+        context['page'] = 1
+        return context
+    
