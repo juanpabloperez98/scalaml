a52212f876ee2236e1e838397ec6e1b525204459
Juan pablo perez <juanpablo.perez@utp.edu.co>
2022-03-07 16:07:17 -0500

    Adding second example

--- apps/type_consistency/urls.py
+++ apps/type_consistency/urls.py
@@ -5,5 +5,7 @@ from .views import *
 app_name = 'type_consistency'
 
 urlpatterns = [
-    path('type_consistency', type_consistency.as_view(), name = 'main')
+    path('type_consistency', type_consistency.as_view(), name = 'main'),
+    path('type_consistency/ejemplo1', ejemplo1.as_view(), name = 'ejemplo1'),
+    path('type_consistency/ejemplo2', ejemplo2.as_view(), name = 'ejemplo2')
 ]
--- apps/type_consistency/views.py
+++ apps/type_consistency/views.py
@@ -10,3 +10,23 @@ from django.views.generic import(
 
 class type_consistency(TemplateView):
     template_name = "type_consistency/index.html"
+
+class ejemplo1(TemplateView):
+    template_name = "type_consistency/ejemplos/ejemplo1.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['example'] = True
+        context['num_example'] = 1
+        context['page'] = 2
+        return context
+
+class ejemplo2(TemplateView):
+    template_name = "type_consistency/ejemplos/ejemplo2.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['example'] = True
+        context['num_example'] = 2
+        context['page'] = 2
+        return context
