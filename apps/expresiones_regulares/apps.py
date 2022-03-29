d07ff7d8b0846b45fbdf9d2907da344657f9e6db
Juan pablo <juanpabloperezdevelopment@gmail.com>
2022-03-29 13:12:27 -0500

    advances 29-03-2022
    Delting old files

--- apps/expresiones_regulares/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExpresionesRegularesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.expresiones_regulares'
--- apps/expresiones_regulares/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = 'expresiones_regulares'
-
-urlpatterns = [
-    path('expresiones_regulares', Main.as_view(), name = 'main'),
-]
